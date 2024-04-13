2ea3a99344d76e1c1beba7712c6428cd517fb838
jpce007 <150888284+jpce007@users.noreply.github.com>
2024-04-13 13:28:45 -0600

    lsito para probar

---
+++ m1.c
@@ -0,0 +1,237 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+#define longitudmax 256
+//por que 256?
+// 255 es la longitud maxima del nombre un archivo en LINUX
+//
+long mv_pid(int pid) {
+    char path[longitudmax];
+    FILE *archistatus;
+    char L[256];
+	//para guardar linea por liena, el max de una liena en LINUX 
+	//es 255 char's de ahi el proque 256 
+    long mv = -1;
+
+    // el path como tal se guarda en el arreglo de char's (buffer), "path"
+    snprintf(path, longitudmax, "/proc/%d/status", pid);
+  //snprintf(path, longitudmax, "/proc/%d/status", pid);
+
+    //la longitud del path es como para prever un nombre super largo 
+    archistatus = fopen(path, "r");
+    if (archistatus == NULL) {
+        perror("! open status");
+        return -1;
+    }
+
+    // linea X liena a no importa el tamano, por eso se usa: L[256]
+    while (fgets(L, sizeof(L), archistatus)) {
+        // Traduccion: si los primeros 7 char's son ==, entre al ->if<-
+        if (strncmp(L, "VmSize:", 7) == 0) {
+             //Saca el tamaño en kilobytes 
+	    //%=Okay "sscanf" le voy a dar un formato para que interprete
+	   //*=Okay "sscanf" lo siguinete lo va a ignorar
+	  //s=Okay "sscanf" ignore \n y \t
+	 //Okay y guarde eso en mvpid
+            sscanf(L, "%*s %ld", &mv);
+            break;//sale para no segir iternado
+        }
+    }
+    fclose(archistatus);
+    return mv;
+}
+
+
+long Tmv() {
+//Okay la memoria vitula TOTAL no esta directamnete definida ya que el uso de la misam es una tecnica de 
+//manejo de memoria para poder sobre pasar la memoria fisica. El OS le asigna memoria vitula a los PID's que estan corriendo y si
+// y si sumamos toda la memoria virtula de todos los PID corriendo ese seria el total de memoria virtula del sistema.
+
+//Eso es lo que se implementa ahora:
+    DIR *proc = opendir("/proc");
+    struct dirent *entry; //se usa para reprecentar el dir
+    long Tmv = 0;
+    long Tmv_too_add = -1;
+    
+  	if (proc == NULL) {
+  		perror("! open /proc");
+  		return 1;
+  	}
+
+    while((entry=readdir(proc))!=NULL){//Iterando pro cada emmtrada del dir 
+        if(entry->d_type==DT_DIR){//viendo a ver si es DT_DIR
+            //si si es un dir enonces segido de eso va un pid, si ese pid esta corriendo el entry->d_name va a ser != 0 entonces tiene memoria virtual en uso 
+            // o sea si el PID ya termino eso quiere decir que no usa memoria virtual, por ahi va la logica de este loop
+            int pid =atoi(entry->d_name);
+            if(pid!=0){//si el name es != 0 entonces 
+                Tmv_too_add = mv_pid(pid);
+                    if (Tmv_too_add != -1) {
+                        Tmv=Tmv+Tmv_too_add;
+                    }
+            }
+        }
+    }
+    closedir(proc);
+
+    return Tmv;
+
+}
+
+long mf_pid(int pid) {
+    char path[longitudmax];
+    FILE *archistatus;
+    char L[256];
+    long mfpid = -1;
+
+    snprintf(path, longitudmax, "/proc/%d/status", pid);
+
+    archistatus = fopen(path, "r");
+    if (archistatus == NULL) {
+        perror("! open status");
+        return -1;
+    }
+
+    while (fgets(L, sizeof(L), archistatus)) {
+        if (strncmp(L, "VmRSS:", 6) == 0) {//esa parte es Mem fisica
+            sscanf(L, "%*s %ld", &mfpid);
+            break;
+        }
+    }
+    fclose(archistatus);
+    return mfpid;
+}
+
+
+long Tmf() {
+  FILE *proc;
+  char path[longitudmax];
+  char L[256];
+  long Tmf = -1;
+
+  snprintf(path, longitudmax, "/proc/meminfo");
+  proc = fopen(path, "r");
+  if (proc == NULL) {
+    perror("! open meminfo");
+    return -1;
+  }
+
+  while (fgets(L, sizeof(L), proc)) {
+    if (strncmp(L, "MemTotal:", 8) == 0) { //total de memoria en RAM fisico 
+      sscanf(L, "%*s %ld", &Tmf);
+      break;
+    }
+  }
+
+  fclose(proc);
+  return Tmf;
+}
+//Todos los metodos comparten muchas similitudes en temas de fromato, solo se lee la info de archivos diferentes
+
+//Todos los metodos comparten muchas similitudes en temas de fromato, solo se lee la info de archivos diferentes
+
+int main() {
+    //memoria virtual----------------------------------------------------------------------------------------------------------------------------------------
+    FILE *proc_file;///para leer 1, y solo 1 archivo de proce con un pid valido
+    //por eso despues abro proc_dir, que se usa para ver si el PID se va a poder usar para jugar con un proc file, ya que si el
+    //pid no es valido no se puede
+    char path[longitudmax];
+    int pid;
+    long TOTALmv=Tmv();
+    long mvpid;
+    char nom[longitudmax];
+    printf("\n\n\n");
+    printf("Memoria Virtual");
+    printf("[Total de memoria virtual: %ld KB]\n", TOTALmv);
+    printf("------------------------------------------------------------------\n\n");
+    printf("PID\t Nombre\t \t Porcentaje de Utilización de Memoria Virtual\n\n");
+    printf("------------------------------------------------------------------\n\n");
+    ////////////////////////////////////////ABRE////////////////////////
+    DIR *proc_dir = opendir("/proc");// este dir solo se usa para filtrar los PID que existen en proc los que estan corriendo
+    struct dirent *entry; //se usa para reprecentar el dir
+
+    
+  	if (proc_dir == NULL) {
+  		perror("! open /proc");
+  		return 1;
+  	}
+    //okay readdir hace eso mismo read dir y regrsa un puntero hacia el sigient sub dir de /proc
+    //es como iterar en el dir /proc los dir que tiene dentro  
+    while((entry=readdir(proc_dir))!=NULL){
+        if(entry->d_type==DT_DIR){
+            int pid =atoi(entry->d_name);
+            if(pid!=0){
+                mvpid = mv_pid(pid);
+                    if (mvpid != -1) {
+                        //comm: para sacar el nombre del proceso
+                        snprintf(path, longitudmax, "/proc/%d/comm", pid);
+                        proc_file = fopen(path, "r");////////////////////////////////////////ABRE////////////////////////
+                        if (proc_file != NULL) {
+                            fgets(nom, sizeof(nom), proc_file);//solo para sacar 1 valor, que de echo es el NOMBRE y esta en la primera linea
+                            fclose(proc_file);///////////////////////////////////////////////CIERA///////////////////////
+                            // Calcular el porcentaje de utilización de memoria virtual
+                            //porceU = porcentaje de Utilizacion 
+                            double porceU = (double)mvpid / TOTALmv * 100.0;
+                            // Imprimir la información del proceso
+                            printf("--------------------------------------------------------------------------\n");
+                            printf("PID:%d\t| Nombre:%s\t| Porcentaje:%.2f%%\n", pid, nom, porceU);
+                            printf("--------------------------------------------------------------------------\n");
+                            //porceU = porcentaje de Utilizacion 
+                        }
+                    }
+            }
+        }
+    }
+    closedir(proc_dir);///////////////////////////////////////////////CIERA///////////////////////
+
+    printf("\n\n\n");
+    printf("Memoria Fisica");
+    //memoria fisica----------------------------------------------------------------------------------------------------------------------------------------
+    long TOTALmf=Tmf();
+    long mfpid;
+    printf("[Total de memoria fisica: %ld KB]\n", TOTALmf);
+    printf("------------------------------------------------------------------\n\n");
+    printf("PID\t Nombre\t \t Porcentaje de Utilización de Memoria Fisica\n\n");
+    printf("------------------------------------------------------------------\n\n");
+
+    proc_dir = opendir("/proc");////////////////////////////////////////ABRE////////////////////////
+
+    
+  	if (proc_dir == NULL) {
+  		perror("! open /proc");
+  		return 1;
+  	}
+    //okay readdir hace eso mismo read dir y regrsa un puntero hacia el sigient sub dir de /proc
+    //es como iterar en el dir /proc los dir que tiene dentro  
+    while((entry=readdir(proc_dir))!=NULL){
+        if(entry->d_type==DT_DIR){
+            int pid =atoi(entry->d_name);
+            if(pid!=0){
+                mfpid = mf_pid(pid);
+                    if (mfpid != -1) {
+                        //comm: para sacar el nombre del proceso
+                        snprintf(path, longitudmax, "/proc/%d/comm", pid);
+                        proc_file = fopen(path, "r");////////////////////////////////////////ABRE////////////////////////
+                        if (proc_file != NULL) {
+                            fgets(nom, sizeof(nom), proc_file);
+                            fclose(proc_file);///////////////////////////////////////////////CIERA///////////////////////
+                            // Calcular el porcentaje de utilización de memoria fisica
+                            double porceUf = (double)mfpid / TOTALmf * 100.0;
+                            // Imprimir la información del proceso
+                            printf("--------------------------------------------------------------------------\n");
+                            printf("PID:%d\t| Nombre:%s\t| Porcentaje:%.2f%%\n", pid, nom, porceUf);
+                            printf("--------------------------------------------------------------------------\n");
+                        }
+                    }
+            }
+        }
+    }
+    closedir(proc_dir);///////////////////////////////////////////////CIERA///////////////////////
+
+    return 0;
+}
